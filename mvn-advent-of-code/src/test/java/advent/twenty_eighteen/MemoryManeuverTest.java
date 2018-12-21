98aad13d0acf4a6497be195574b4e122914b6e30
Andrew Norman <akiwicoder@gmail.com>
2018-12-21 08:44:41 +0000

    Adding 2018 Day 8

---
+++ mvn-advent-of-code/src/test/java/advent/twenty_eighteen/MemoryManeuverTest.java
@@ -0,0 +1,23 @@
+package advent.twenty_eighteen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MemoryManeuverTest {
+    @Test
+    public void checkReal() {
+        MemoryManeuver im = new  MemoryManeuver("/twenty_eighteen/MemoryManeuver-input.txt");
+
+        assertEquals(43825, im.getPart1Answer().intValue());
+        assertEquals(19276, im.getPart2Answer().intValue());
+    }
+
+    @Test
+    public void checkExample1() {
+        MemoryManeuver im = new  MemoryManeuver("/twenty_eighteen/MemoryManeuver-example#1.txt");
+
+        assertEquals(138, im.getPart1Answer().intValue());
+        assertEquals(66, im.getPart2Answer().intValue());
+    }
+}
