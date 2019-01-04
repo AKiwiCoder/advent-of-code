f35ebb727ff2332492881f01df53c8a9eec333f2
Andrew Norman <akiwicoder@gmail.com>
2019-01-04 21:53:06 +0000

    Adding 2015 Day 17

---
+++ mvn-advent-of-code/src/main/java/advent/twenty_fifteen/Day17NoSuchThingAsTooMuch.java
@@ -0,0 +1,117 @@
+package advent.twenty_fifteen;
+
+import advent.common.DailyProblem;
+import advent.utilities.FileUtilities;
+
+import java.util.*;
+
+public class Day17NoSuchThingAsTooMuch implements DailyProblem<Integer, Integer> {
+    private final int part1Answer;
+    private final int part2Answer;
+
+    static class Container implements Comparable<Container> {
+        final int volume;
+
+        Container(int volume) {
+            this.volume = volume;
+        }
+
+        @Override
+        public String toString() {
+            return volume + " (" + hashCode() + ")";
+        }
+
+        @Override
+        public int compareTo(Container o) {
+            int c = Integer.compare(volume, o.volume);
+            if (c == 0) {
+                return Integer.compare(hashCode(), o.hashCode());
+            }
+            return c;
+        }
+    }
+
+    public Day17NoSuchThingAsTooMuch(String filename, int target) {
+        List<Container> containers = FileUtilities.readLines(filename, s -> new Container(Integer.parseInt(s)));
+
+        int[] volumes = new int[containers.size()];
+        BitSet remaining = new BitSet(containers.size());
+        for (int i = 0; i != containers.size(); i++) {
+            volumes[i] = containers.get(i).volume;
+            remaining.set(i);
+        }
+
+        Set<BitSet> groups = generate(volumes, new BitSet(), remaining, 0, target);
+
+        this.part1Answer = groups.size();
+
+        Map<Integer, Integer> counts = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        for (BitSet g : groups) {
+            int key = g.cardinality();
+            if (key < min) {
+                min = key;
+            }
+            counts.put(key, counts.getOrDefault(key, 0) + 1);
+        }
+
+        this.part2Answer = counts.get(min);
+    }
+
+    private static Set<BitSet> generate(int[] volumes, BitSet soFar, BitSet remaining, int sum, int target) {
+        Set<BitSet> result = new HashSet<>();
+        for (int b = 0; b != remaining.length(); b++) {
+            if (remaining.get(b)) {
+                if (volumes[b] + sum > target) {
+                    continue;
+                }
+
+                BitSet newSoFar = (BitSet) soFar.clone();
+                newSoFar.set(b);
+
+                if (volumes[b] + sum == target) {
+                    result.add(newSoFar);
+                    continue;
+                }
+
+                BitSet newRemaining = (BitSet) remaining.clone();
+                newRemaining.clear(b);
+                result.addAll(generate(volumes, newSoFar, newRemaining, sum + volumes[b], target));
+            }
+        }
+        return result;
+    }
+
+    private static Set<List<Container>> generate(List<Container> soFar, List<Container> remaining, int sum, int target) {
+        Set<List<Container>> result = new HashSet<>();
+        for (Container container : remaining) {
+            if (container.volume + sum > target) {
+                continue;
+            }
+
+            List<Container> newSoFar = new LinkedList<>(soFar);
+            newSoFar.add(container);
+            Collections.sort(newSoFar);
+
+            if (container.volume + sum == target) {
+                result.add(newSoFar);
+                continue;
+            }
+
+            List<Container> newRemaining = new LinkedList<>(remaining);
+            newRemaining.remove(container);
+            result.addAll(generate(newSoFar, newRemaining, sum + container.volume, target));
+        }
+        return result;
+    }
+
+    @Override
+    public Integer getPart1Answer() {
+        return part1Answer;
+    }
+
+    @Override
+    public Integer getPart2Answer() {
+        return part2Answer;
+    }
+}
---
+++ mvn-advent-of-code/src/test/java/advent/twenty_fifteen/Day17NoSuchThingAsTooMuchTest.java
@@ -0,0 +1,24 @@
+package advent.twenty_fifteen;
+
+import advent.common.DailyProblem;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Day17NoSuchThingAsTooMuchTest {
+    @Test
+    public void checkReal() {
+        DailyProblem<Integer, Integer> dp = new Day17NoSuchThingAsTooMuch("/twenty_fifteen/Day17-NoSuchThingAsTooMuch-input.txt", 150);
+
+        assertEquals(1304, dp.getPart1Answer().intValue());
+        assertEquals(18, dp.getPart2Answer().intValue());
+    }
+
+    @Test
+    public void checkExample1() {
+        DailyProblem<Integer, Integer> dp = new Day17NoSuchThingAsTooMuch("/twenty_fifteen/Day17-NoSuchThingAsTooMuch-example#1.txt", 25);
+
+        assertEquals(4, dp.getPart1Answer().intValue());
+        assertEquals(3, dp.getPart2Answer().intValue());
+    }
+}
