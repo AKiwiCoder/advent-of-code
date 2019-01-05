635a43c043826e721fb15833d7d0d51edb5aa5ea
Andrew Norman <akiwicoder@gmail.com>
2019-01-05 22:13:35 +0000

    Removed System.out.println() from a test

--- mvn-advent-of-code/src/test/java/advent/utilities/ListUtilitiesTest.java
+++ mvn-advent-of-code/src/test/java/advent/utilities/ListUtilitiesTest.java
@@ -19,19 +19,15 @@ public class ListUtilitiesTest {
     @Test
     public void testGenerateCombinations() {
         List<List<String>> test0 = ListUtilities.generateCombinations(null, EMPTY, null);
-        System.out.println(test0);
         assertEquals(0, test0.size());
 
         List<List<String>> test1 = ListUtilities.generateCombinations(null, ONE, null);
-        System.out.println(test1);
         assertEquals(1, test1.size());
 
         List<List<String>> test2 = ListUtilities.generateCombinations(null, TWO, null);
-        System.out.println(test2);
         assertEquals(2, test2.size());
 
         List<List<String>> test3 = ListUtilities.generateCombinations(null, THREE, null);
-        System.out.println(test3);
         assertEquals(6, test3.size());
     }
 }
