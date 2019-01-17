a483e6589c9c8f6371534e39c1c7255255f98cca
Andrew Norman <akiwicoder@gmail.com>
2019-01-17 20:36:54 +0000

    Scala - Adding 2015 Day 02

---
+++ scala-advent-of-code/src/main/scala/advent/twenty_fifteen/Day02IWasToldThereWouldBeNoMath.scala
@@ -0,0 +1,24 @@
+package advent.twenty_fifteen
+
+import advent.common.DailyProblem
+import advent.utilities.FileUtilities
+
+case class Parcel(length: Int, width: Int, height: Int)
+
+class Day02IWasToldThereWouldBeNoMath(filename: String) extends DailyProblem[Int, Int] {
+  private val parcels = FileUtilities.readFile(filename, parse)
+
+  private def parse(line: String): Parcel = {
+    val bits = line.split("x")
+    Parcel(bits(0).toInt, bits(1).toInt, bits(2).toInt)
+  }
+
+  private def surfaceArea(p : Parcel): Int = 2 * p.length * p.width + 2 * p.width * p.height + 2 * p.height * p.length
+  private def surfaceAreaOfSmallestSide(p : Parcel): Int = Math.min(p.height * p.width, Math.min(p.height * p.length, p.length * p.width))
+
+  private def lengthToWrap(p: Parcel) = Math.min(2 * p.height + 2 * p.width, Math.min(2 * p.height + 2 * p.length, 2 * p.length + 2 * p.width))
+  private def lengthOfBow(p: Parcel) = p.width * p.length * p.height
+
+  override val part1Answer = parcels.foldLeft(0)((a,p) => a + surfaceArea(p) + surfaceAreaOfSmallestSide(p))
+  override val part2Answer = parcels.foldLeft(0)((a,p) => a + lengthToWrap(p) + lengthOfBow(p))
+}
