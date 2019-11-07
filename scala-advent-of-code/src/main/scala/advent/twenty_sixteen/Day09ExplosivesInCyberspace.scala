344bee599fd247c531ede3b8c9c9b476a5bf1987
akiwicoder <akiwicoder@gmail.com>
2019-11-07 07:59:46 +0000

    Adding Scala 2016 Day 9

--- scala-advent-of-code/src/main/scala/advent/twenty_sixteen/Day09ExplosivesInCyberspace.scala
+++ scala-advent-of-code/src/main/scala/advent/twenty_sixteen/Day09ExplosivesInCyberspace.scala
@@ -1,5 +1,37 @@
 package advent.twenty_sixteen
 
-class Day09ExplosivesInCyberspace {
+import advent.common.DailyProblem
+import advent.utilities.FileUtilities
 
+class Day09ExplosivesInCyberspace(filename: String) extends DailyProblem[Long, Long] {
+
+  private val lines = FileUtilities.readFile(filename)
+
+  private def decompress(line : String, expandSubstring : Boolean) : Long = {
+    if (line.isEmpty) {
+      0
+    } else {
+      val markerStart = line.indexOf('(')
+      if (markerStart < 0) {
+        line.length
+      } else if (markerStart == 0) {
+        val xpos = line.indexOf('x')
+        val length = line.substring(1, xpos).toInt
+        val markerClose = line.indexOf(')')
+        val count = line.substring(xpos+1, markerClose).toInt
+
+        val substring = line.drop(markerClose + 1).take(length)
+        if (expandSubstring && substring.contains("(")) {
+          decompress(substring, expandSubstring) * count + decompress(line.drop(markerClose + 1 + length), expandSubstring)
+        } else {
+          length * count + decompress(line.drop(markerClose + 1 + length), expandSubstring)
+        }
+      } else {
+        markerStart + decompress(line.drop(markerStart), expandSubstring)
+      }
+    }
+  }
+
+  override val part1Answer: Long = lines.map(line => decompress(line, false)).sum
+  override val part2Answer: Long = lines.map(line => decompress(line, true)).sum
 }
--- scala-advent-of-code/src/test/scala/advent/twenty_sixteen/Day09ExplosivesInCyberspaceTest.scala
+++ scala-advent-of-code/src/test/scala/advent/twenty_sixteen/Day09ExplosivesInCyberspaceTest.scala
@@ -6,15 +6,15 @@ class Day09ExplosivesInCyberspaceTest extends FlatSpec with Matchers {
   "2016 Day 9 - Input" should "calculate answers" in {
     val dp = new Day09ExplosivesInCyberspace("/twenty_sixteen/Day09-ExplosivesInCyberspace-input.txt")
 
-    dp.part1Answer should be(110)
-    dp.part2Answer should be(242)
+    dp.part1Answer should be(70186L)
+    dp.part2Answer should be(10915059201L)
   }
 
   "2016 Day 9 - Example #1" should "calculate answers" in {
     val dp = new Day09ExplosivesInCyberspace("/twenty_sixteen/Day09-ExplosivesInCyberspace-example#1.txt")
 
-    dp.part1Answer should be(2)
-    dp.part2Answer should be(0)
+    dp.part1Answer should be(57L)
+    dp.part2Answer should be(56L)
   }
 
 }
