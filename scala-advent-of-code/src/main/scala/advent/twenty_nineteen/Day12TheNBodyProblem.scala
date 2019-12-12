34ba3e4df840535fcb2ff8526e9f35264bec4d4b
akiwicoder <akiwicoder@gmail.com>
2019-12-12 14:30:59 +0000

    Some renaming and refactoring to make things clearer

--- scala-advent-of-code/src/main/scala/advent/twenty_nineteen/Day12TheNBodyProblem.scala
+++ scala-advent-of-code/src/main/scala/advent/twenty_nineteen/Day12TheNBodyProblem.scala
@@ -3,9 +3,19 @@ package advent.twenty_nineteen
 import advent.common.DailyProblem
 import advent.utilities.FileUtilities
 
-case class Body(x: Int, y: Int, z: Int)
+import scala.annotation.tailrec
 
-case class Velocity(x: Int, y: Int, z: Int)
+case class Body(x: Int, y: Int, z: Int) {
+  def calculateEnergy(): Int = {
+    Math.abs(x) + Math.abs(y) + Math.abs(z)
+  }
+}
+
+case class Velocity(x: Int, y: Int, z: Int) {
+  def calculateEnergy(): Int = {
+    Math.abs(x) + Math.abs(y) + Math.abs(z)
+  }
+}
 
 class Day12TheNBodyProblem(filename: String, steps: Int) extends DailyProblem[Int, Long] {
 
@@ -19,100 +29,89 @@ class Day12TheNBodyProblem(filename: String, steps: Int) extends DailyProblem[In
 
   private val moons = FileUtilities.readFile(filename, parser).zipWithIndex.map(a => a._2 -> a._1).toMap
 
-  def ifladder(m: Int, t: Int): Int = {
-    if (m > t) {
-      -1
-    } else if (m < t) {
-      1
-    } else {
-      0
-    }
+  def applyGravity(m: Int, t: Int): Int = if (m > t) -1 else if (m < t) 1 else 0
+
+  def calculateGravity(velocity: Velocity, me: Body, them: Body): Velocity = {
+    val x = velocity.x + applyGravity(me.x, them.x)
+    val y = velocity.y + applyGravity(me.y, them.y)
+    val z = velocity.z + applyGravity(me.z, them.z)
+    Velocity(x, y, z)
   }
 
-  def calculate(velocity: Velocity, me: Body, them: Body): Velocity = {
-    val x = velocity.x + ifladder(me.x, them.x)
-    val y = velocity.y + ifladder(me.y, them.y)
-    val z = velocity.z + ifladder(me.z, them.z)
-    Velocity(x, y, z)
+  def calculateGravity(me: (Body, Velocity), them: (Body, Velocity)): Velocity = {
+    calculateGravity(me._2, me._1, them._1)
   }
 
   private def doStep(input: Map[Int, (Body, Velocity)]): Map[Int, (Body, Velocity)] = {
     var bodies = input
-    val pairs = bodies.keySet.toList.combinations(2).toList
-    for (b <- pairs) {
+    val idPairList = bodies.keySet.toList.combinations(2).toList
 
-      val b1 = b(0)
-      val b2 = b(1)
+    for (idPair <- idPairList) {
+      val id0 = idPair(0)
+      val id1 = idPair(1)
 
-      val b1p = bodies(b1)._1
-      val b2p = bodies(b2)._1
-      val b1v = bodies(b1)._2
-      val b2v = bodies(b2)._2
+      val newVelocity0 = calculateGravity(bodies(id0), bodies(id1))
+      val newVelocity1 = calculateGravity(bodies(id1), bodies(id0))
 
-      val nb1v = calculate(b1v, b1p, b2p)
-      val nb2v = calculate(b2v, b2p, b1p)
-
-      bodies = bodies + (b1 -> (bodies(b1)._1, nb1v))
-      bodies = bodies + (b2 -> (bodies(b2)._1, nb2v))
+      bodies = bodies + (id0 -> (bodies(id0)._1, newVelocity0))
+      bodies = bodies + (id1 -> (bodies(id1)._1, newVelocity1))
     }
 
     bodies.map(a => (a._1, (Body(a._2._1.x + a._2._2.x, a._2._1.y + a._2._2.y, a._2._1.z + a._2._2.z), a._2._2)))
   }
 
   private def calculateEnergy(input: Map[Int, (Body, Velocity)]): Int = {
-    var working = input
-    for (step <- 0 until steps) {
-      working = doStep(working)
+    @tailrec
+    def execute(step: Int, working: Map[Int, (Body, Velocity)]): Map[Int, (Body, Velocity)] = {
+      if (step == 0) {
+        working
+      } else {
+        execute(step - 1, doStep(working))
+      }
     }
-    val energy = working.values.map(a => (Math.abs(a._1.x) + Math.abs(a._1.y) + Math.abs(a._1.z)) * ((Math.abs(a._2.x) + Math.abs(a._2.y) + Math.abs(a._2.z)))).sum
-    energy
+
+    execute(steps, input).values.map(a => a._1.calculateEnergy() * a._2.calculateEnergy()).sum
   }
 
-  private def gen(i: Int, bodies: List[(Body, Velocity)]): (Int, Int, Int, Int) = {
-    i match {
-      case 0 => (bodies(0)._1.x, bodies(1)._1.x, bodies(2)._1.x, bodies(3)._1.x)
-      case 1 => (bodies(0)._1.y, bodies(1)._1.y, bodies(2)._1.y, bodies(3)._1.y)
-      case 2 => (bodies(0)._1.z, bodies(1)._1.z, bodies(2)._1.z, bodies(3)._1.z)
-      case 3 => (bodies(0)._2.x, bodies(1)._2.x, bodies(2)._2.x, bodies(3)._2.x)
-      case 4 => (bodies(0)._2.y, bodies(1)._2.y, bodies(2)._2.y, bodies(3)._2.y)
-      case 5 => (bodies(0)._2.z, bodies(1)._2.z, bodies(2)._2.z, bodies(3)._2.z)
-    }
+  private def generateKey(i: Int, bodies: List[(Body, Velocity)]): String = {
+    bodies.map(body => i match {
+      case 0 => body._1.x
+      case 1 => body._1.y
+      case 2 => body._1.z
+      case 3 => body._2.x
+      case 4 => body._2.y
+      case 5 => body._2.z
+    }).mkString("|")
   }
 
-  private def lcm(list: Seq[Long]): Long = list.foldLeft(1: Long) {
-    (a, b) => b * a / Stream.iterate((a, b)) { case (x, y) => (y, x % y) }.dropWhile(_._2 != 0).head._1.abs
-  }
-  
-  private def generateHashCode(i: Int, working: List[(Body, Velocity)]): ((Int, Int, Int, Int), (Int, Int, Int, Int)) = {
-    (gen(i, working), gen(i + 3, working))
+  private def leastCommonMultiple(list: Seq[Long]): Long =
+    list.foldLeft(1: Long) {
+      (a, b) => b * a / Stream.iterate((a, b)) { case (x, y) => (y, x % y) }.dropWhile(_._2 != 0).head._1.abs
+    }
+
+  private def generateHashCode(i: Int, working: List[(Body, Velocity)]): String = {
+    generateKey(i, working) + "|" + generateKey(i + 3, working)
   }
 
   private def calculateCycles(input: Map[Int, (Body, Velocity)]): Long = {
     var cycles = Map[Int, Int]()
-    var seen = Map[Int, Set[((Int, Int, Int, Int), (Int, Int, Int, Int))]]().withDefaultValue(Set())
+    var seen = Map[Int, Set[String]]().withDefaultValue(Set())
     var cycle = 0
     var working = input
+
     while (cycles.size != 3) {
-      val h0 = generateHashCode(0, working.values.toList)
-      val h1 = generateHashCode(1, working.values.toList)
-      val h2 = generateHashCode(2, working.values.toList)
-
-      if (seen(0).contains(h0) && !cycles.contains(0))
-        cycles = cycles + (0 -> cycle)
-      seen = seen + (0 -> (seen(0) + h0))
-
-      if (seen(1).contains(h1) && !cycles.contains(1))
-        cycles = cycles + (1 -> cycle)
-      seen = seen + (1 -> (seen(1) + h1))
-
-      if (seen(2).contains(h2) && !cycles.contains(2))
-        cycles = cycles + (2 -> cycle)
-      seen = seen + (2 -> (seen(2) + h2))
+      for (i <- 0 until 3) {
+        val hashcode = generateHashCode(i, working.values.toList)
+        if (seen(i).contains(hashcode) && !cycles.contains(i))
+          cycles = cycles + (i -> cycle)
+        seen = seen + (i -> (seen(i) + hashcode))
+      }
 
       cycle += 1
       working = doStep(working)
     }
-    lcm(cycles.values.map(i => i.toLong).toList)
+
+    leastCommonMultiple(cycles.values.map(i => i.toLong).toList)
   }
 
   override val part1Answer: Int = calculateEnergy(moons)
