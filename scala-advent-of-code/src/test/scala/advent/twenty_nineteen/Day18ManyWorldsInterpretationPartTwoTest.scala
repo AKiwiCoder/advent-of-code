898dd6f6a5957d030847f7ca65fee56a28c79e9e
akiwicoder <akiwicoder@gmail.com>
2019-12-20 20:03:38 +0000

    Disabling tests that run for 40+ minutes

--- scala-advent-of-code/src/test/scala/advent/twenty_nineteen/Day18ManyWorldsInterpretationPartTwoTest.scala
+++ scala-advent-of-code/src/test/scala/advent/twenty_nineteen/Day18ManyWorldsInterpretationPartTwoTest.scala
@@ -3,12 +3,12 @@ package advent.twenty_nineteen
 import org.scalatest.{FlatSpec, Matchers}
 
 class Day18ManyWorldsInterpretationPartTwoTest extends FlatSpec with Matchers {
-  "2019 Day 18 - Input" should "calculate answers" in {
-    val dp = new Day18ManyWorldsInterpretationPartTwo("/twenty_nineteen/Day18-ManyWorldsInterpretation-input.txt")
-
-    dp.part1Answer should be(2946)
-    dp.part2Answer should be(0)
-  }
+//  "2019 Day 18 - Input" should "calculate answers" in {
+//    val dp = new Day18ManyWorldsInterpretationPartTwo("/twenty_nineteen/Day18-ManyWorldsInterpretation-input.txt")
+//
+//    dp.part1Answer should be(2946)
+//    dp.part2Answer should be(0)
+//  }
 
   "2019 Day 18 - Example #6" should "calculate answers" in {
     val dp = new Day18ManyWorldsInterpretationPartTwo("/twenty_nineteen/Day18-ManyWorldsInterpretation-example#6.txt")
--- scala-advent-of-code/src/test/scala/advent/twenty_nineteen/Day18ManyWorldsInterpretationTest.scala
+++ scala-advent-of-code/src/test/scala/advent/twenty_nineteen/Day18ManyWorldsInterpretationTest.scala
@@ -3,12 +3,12 @@ package advent.twenty_nineteen
 import org.scalatest.{FlatSpec, Matchers}
 
 class Day18ManyWorldsInterpretationTest extends FlatSpec with Matchers {
-  "2019 Day 18 - Input" should "calculate answers" in {
-    val dp = new Day18ManyWorldsInterpretation("/twenty_nineteen/Day18-ManyWorldsInterpretation-input.txt")
-
-    dp.part1Answer should be(2946)
-    dp.part2Answer should be(0)
-  }
+//  "2019 Day 18 - Input" should "calculate answers" in {
+//    val dp = new Day18ManyWorldsInterpretation("/twenty_nineteen/Day18-ManyWorldsInterpretation-input.txt")
+//
+//    dp.part1Answer should be(2946)
+//    dp.part2Answer should be(0)
+//  }
 
   "2019 Day 18 - Example #1" should "calculate answers" in {
     val dp = new Day18ManyWorldsInterpretation("/twenty_nineteen/Day18-ManyWorldsInterpretation-example#1.txt")
