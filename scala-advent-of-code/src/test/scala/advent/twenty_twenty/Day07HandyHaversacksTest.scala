901ec026d88b9c6cb4758924d1fc9beed5c91a18
Andrew Norman <akiwicoder@gmail.com>
2020-12-07 08:30:29 +0000

    Adding Scala 2020 Day 7

---
+++ scala-advent-of-code/src/test/scala/advent/twenty_twenty/Day07HandyHaversacksTest.scala
@@ -0,0 +1,28 @@
+package advent.twenty_twenty
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Day07HandyHaversacksTest extends FlatSpec with Matchers {
+  "2020 Day 07 - Input" should "calculate answers" in {
+    val dp = new Day07HandyHaversacks("/twenty_twenty/Day07-HandyHaversacks-input.txt")
+
+    dp.part1Answer should be(278)
+    dp.part2Answer should be(0)
+  }
+
+  "2020 Day 07 - Example #1" should "calculate answers" in {
+    val dp = new Day07HandyHaversacks("/twenty_twenty/Day07-HandyHaversacks-example#1.txt")
+
+    dp.part1Answer should be(4)
+    dp.part2Answer should be(32)
+  }
+
+  "2020 Day 07 - Example #2" should "calculate answers" in {
+    val dp = new Day07HandyHaversacks("/twenty_twenty/Day07-HandyHaversacks-example#2.txt")
+
+    dp.part1Answer should be(0)
+    dp.part2Answer should be(126)
+  }
+}
+
+
