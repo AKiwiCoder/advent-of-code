99618c9e72403514720d62583063914ea329cdad
Andrew Norman <akiwicoder@gmail.com>
2020-12-08 07:06:13 +0000

    Adding Scala 2020 Day 8

---
+++ scala-advent-of-code/src/test/scala/advent/twenty_twenty/Day08HandheldHaltingTest.scala
@@ -0,0 +1,21 @@
+package advent.twenty_twenty 
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Day08HandheldHaltingTest extends FlatSpec with Matchers {
+  "2020 Day 08 - Input" should "calculate answers" in {
+    val dp = new Day08HandheldHalting("/twenty_twenty/Day08-HandheldHalting-input.txt")
+
+    dp.part1Answer should be(2058)
+    dp.part2Answer should be(0)
+  }
+
+  "2020 Day 08 - Example #1" should "calculate answers" in {
+    val dp = new Day08HandheldHalting("/twenty_twenty/Day08-HandheldHalting-example#1.txt")
+
+    dp.part1Answer should be(5)
+    dp.part2Answer should be(8)
+  }
+}
+
+
