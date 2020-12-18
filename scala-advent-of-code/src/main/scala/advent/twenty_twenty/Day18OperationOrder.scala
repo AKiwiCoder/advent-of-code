2441eaec29e78e3cdf09eb9e5a87d60e25dc1f62
Andrew Norman <akiwicoder@gmail.com>
2020-12-18 13:12:52 +0000

    Refactoring Scala 2020 Day 18

--- scala-advent-of-code/src/main/scala/advent/twenty_twenty/Day18OperationOrder.scala
+++ scala-advent-of-code/src/main/scala/advent/twenty_twenty/Day18OperationOrder.scala
@@ -6,9 +6,6 @@ import advent.utilities.FileUtilities
 import scala.annotation.tailrec
 
 class Day18OperationOrder(filename: String) extends DailyProblem[Long, Long] {
-
-  private val input = FileUtilities.readFile(filename)
-
   abstract class Token
   case class Number(num: Long) extends Token
   case class Operator(op: Char) extends Token
@@ -37,6 +34,8 @@ class Day18OperationOrder(filename: String) extends DailyProblem[Long, Long] {
     }
   }
 
+  def parse(line : String) : List[Token] = tokenize(line.toList, "", List()).reverse
+
   def evaluate(rpn : List[Token], stack : List[Number]) : Long = {
     if (rpn.isEmpty) {
       stack.head.num
@@ -54,97 +53,55 @@ class Day18OperationOrder(filename: String) extends DailyProblem[Long, Long] {
     }
   }
 
-  def process(line: String): Long = {
-    @tailrec
-    def rpn(tokens: List[Token], output: List[Token], operators: List[Token]): List[Token] = {
-      if (tokens.isEmpty) {
-        if (operators.isEmpty) {
-          output
-        } else {
-          operators ::: output
-        }
+  def equalsPrecedence(target: Token, current : Token): Boolean = true
+
+  def mixedPrecedence(target: Token, current : Token): Boolean = {
+    val cOp = current.asInstanceOf[Operator]
+    val tOp = target.asInstanceOf[Operator]
+    if ((tOp.op == '+' || tOp.op == '-') && (cOp.op == '+' || cOp.op == '-')) {
+      true
+    } else if ((tOp.op == '*' || tOp.op == '/') && (cOp.op == '*' || cOp.op == '/')) {
+      true
+    } else if ((tOp.op == '+' || tOp.op == '-') && (cOp.op == '*' || cOp.op == '/')) {
+      false
+    } else {
+      true
+    }
+  }
+
+  @tailrec
+   private def rpn(tokens: List[Token], output: List[Token], operators: List[Token], precedence: (Token,Token) => Boolean): List[Token] = {
+    if (tokens.isEmpty) {
+      if (operators.isEmpty) {
+        (output).reverse
       } else {
-        val token = tokens.head
-        token match {
-          case Number(_) => rpn(tokens.tail, token :: output, operators)
-          case Operator(_) => {
-            val toPop = operators.takeWhile(operator => !operator.isInstanceOf[LeftBracket])
-            val newOperators = operators.drop(toPop.size)
-            rpn(tokens.tail, toPop ::: output, token :: newOperators)
-          }
-          case LeftBracket() => rpn(tokens.tail, output, token :: operators)
-          case RightBracket() => {
-            val inside = operators.takeWhile(operator => !operator.isInstanceOf[LeftBracket])
-            val newOperators = operators.drop(inside.size + 1)
-            rpn(tokens.tail, inside ::: output, newOperators)
-          }
+        (operators.reverse ::: output).reverse
+      }
+    } else {
+      val token = tokens.head
+      token match {
+        case Number(_) => rpn(tokens.tail, token :: output, operators, precedence)
+        case Operator(_) => {
+          val toPop = operators.takeWhile(operator => !operator.isInstanceOf[LeftBracket] && precedence(token, operator)).reverse
+          val newOperators = operators.drop(toPop.size)
+          rpn(tokens.tail, toPop ::: output, token :: newOperators, precedence)
+        }
+        case LeftBracket() => rpn(tokens.tail, output, token :: operators, precedence)
+        case RightBracket() => {
+          val inside = operators.takeWhile(operator => !operator.isInstanceOf[LeftBracket]).reverse
+          val newOperators = operators.drop(inside.size + 1)
+          rpn(tokens.tail, inside ::: output, newOperators, precedence)
         }
       }
     }
-
-    val tokens = tokenize(line.toList, "", List()).reverse
-    val rpnExpr = rpn(tokens, List(), List()).reverse
-    evaluate(rpnExpr, List())
   }
 
+  private val input = FileUtilities.readFile(filename, parse)
 
-  def process2(line: String): Long = {
-    def shouldPopToken(target: Token, current : Token): Boolean = {
-        val cOp = current.asInstanceOf[Operator]
-        val tOp = target.asInstanceOf[Operator]
-        if ((tOp.op == '+' || tOp.op == '-') && (cOp.op == '+' || cOp.op == '-')) {
-          true
-        } else if ((tOp.op == '*' || tOp.op == '/') && (cOp.op == '*' || cOp.op == '/')) {
-          true
-        } else if ((tOp.op == '+' || tOp.op == '-') && (cOp.op == '*' || cOp.op == '/')) {
-          false
-        } else {
-          true
-        }
-    }
+  println("Input: " + input)
 
-    @tailrec
-    def rpn(tokens: List[Token], output: List[Token], operators: List[Token]): List[Token] = {
-      if (tokens.isEmpty) {
-        if (operators.isEmpty) {
-          output
-        } else {
-          operators.reverse ::: output
-        }
-      } else {
-        val token = tokens.head
-        token match {
-          case Number(_) => rpn(tokens.tail, token :: output, operators)
-          case Operator(_) => {
-            val toPop = operators.takeWhile(operator => !operator.isInstanceOf[LeftBracket] && shouldPopToken(token, operator)).reverse
-            val newOperators = operators.drop(toPop.size)
-            rpn(tokens.tail, toPop ::: output, token :: newOperators)
-          }
-          case LeftBracket() => rpn(tokens.tail, output, token :: operators)
-          case RightBracket() => {
-            val inside = operators.takeWhile(operator => !operator.isInstanceOf[LeftBracket]).reverse
-            val newOperators = operators.drop(inside.size + 1)
-            rpn(tokens.tail, inside ::: output, newOperators)
-          }
-        }
-      }
-    }
-
-    val tokens = tokenize(line.toList, "", List()).reverse
-    val rpnExpr = rpn(tokens, List(), List()).reverse
-    evaluate(rpnExpr, List())
-  }
-
-  private def part1(): Long = {
-    input.map(line => process(line)).sum
-  }
-
-  private def part2(): Long = {
-    input.map(line => process2(line)).sum
-  }
-
-  override val part1Answer: Long = part1()
-  override val part2Answer: Long = part2()
+  override val part1Answer: Long = input.map(tokens => evaluate(rpn(tokens, List(), List(), equalsPrecedence), List())).sum
+  override val part2Answer: Long = input.map(tokens => evaluate(rpn(tokens, List(), List(), mixedPrecedence), List())).sum
 }
 
 
