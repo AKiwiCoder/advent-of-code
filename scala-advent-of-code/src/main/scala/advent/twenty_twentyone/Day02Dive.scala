5c3d074cfaba9120f0cab20373462f2ccdba519e
Andrew Norman <akiwicoder@gmail.com>
2021-12-02 20:42:11 +1100

    Scala 2021 Day 02 - Some code cleanup

--- scala-advent-of-code/src/main/scala/advent/twenty_twentyone/Day02Dive.scala
+++ scala-advent-of-code/src/main/scala/advent/twenty_twentyone/Day02Dive.scala
@@ -14,27 +14,27 @@ class Day02Dive(filename: String) extends DailyProblem[Int, Int] {
   private val down = "down ([0-9])+".r
   private val aim = "aim ([0-9])+".r
 
-  private def perform1(current: (Int, Int), command: String): (Int, Int) = {
+  private def perform1(depth : Int, distance : Int, command: String): (Int, Int) = {
     command match {
-      case forward(s) => (current._1, current._2 + s.toInt)
-      case up(s) => (current._1 - s.toInt, current._2)
-      case down(s) => (current._1 + s.toInt, current._2)
+      case forward(steps) => (depth, distance + steps.toInt)
+      case up(steps) => (depth - steps.toInt, distance)
+      case down(steps) => (depth + steps.toInt, distance)
     }
   }
 
-  private def perform2(current: (Int, Int, Int), command: String): (Int, Int, Int) = {
+  private def perform2(depth : Int, distance : Int, aim : Int, command: String): (Int, Int, Int) = {
     command match {
-      case forward(s) => (current._1 + current._3 * s.toInt, current._2 + s.toInt, current._3)
-      case up(s) => (current._1, current._2, current._3 - s.toInt)
-      case down(s) => (current._1, current._2, current._3 + s.toInt)
+      case forward(steps) => (depth + aim * steps.toInt, distance + steps.toInt, aim)
+      case up(steps) => (depth, distance, aim - steps.toInt)
+      case down(steps) => (depth, distance, aim + steps.toInt)
     }
   }
 
-  private val part1EndPos = input.foldLeft((0, 0))((acc, command) => perform1(acc, command))
-  private val part2EndPos = input.foldLeft((0, 0, 0))((acc, command) => perform2(acc, command))
+  private val (p1depth, p1distance) = input.foldLeft((0, 0))((acc, command) => perform1(acc._1, acc._2, command))
+  private val (p2depth, p2distance, _) = input.foldLeft((0, 0, 0))((acc, command) => perform2(acc._1, acc._2, acc._3, command))
 
-  override val part1Answer: Int = part1EndPos._1 * part1EndPos._2
-  override val part2Answer: Int = part2EndPos._1 * part2EndPos._2
+  override val part1Answer: Int = p1depth * p1distance
+  override val part2Answer: Int = p2depth * p2distance
 }
 
 
