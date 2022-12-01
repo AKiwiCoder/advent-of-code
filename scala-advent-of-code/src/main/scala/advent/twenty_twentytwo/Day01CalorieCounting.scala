26864ac6f7d7b9572df32aaeaf6908f70c7f3656
Andrew Norman <akiwicoder@gmail.com>
2022-12-01 22:23:37 +1100

    Adding Scala 2022 Day 01 - Tidy Up

--- scala-advent-of-code/src/main/scala/advent/twenty_twentytwo/Day01CalorieCounting.scala
+++ scala-advent-of-code/src/main/scala/advent/twenty_twentytwo/Day01CalorieCounting.scala
@@ -9,7 +9,7 @@ class Day01CalorieCounting(filename : String) extends DailyProblem[Int, Int] {
 
   private val input = FileUtilities.readFile(filename)
 
-  val elves = input.foldLeft((0, List[Int]()))((acc, str) => if (str.trim().isEmpty()) (0, acc._1 :: acc._2) else (acc._1 + str.toInt, acc._2))._2.sorted.reverse
+  private val elves = input.foldLeft((0, List[Int]()))((acc, str) => if (str.trim().isEmpty) (0, acc._1 :: acc._2) else (acc._1 + str.toInt, acc._2))._2.sorted.reverse
 
   override val part1Answer: Int = elves.head
   override val part2Answer: Int = elves.take(3).sum
