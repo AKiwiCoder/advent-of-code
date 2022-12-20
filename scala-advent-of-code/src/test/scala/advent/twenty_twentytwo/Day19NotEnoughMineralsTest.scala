984c1fe82431996c04f5e93d572663c8654cdd58
Andrew Norman <akiwicoder@gmail.com>
2022-12-20 21:19:52 +1100

    Scala 2022 Day 19 - Fixed test names

--- scala-advent-of-code/src/test/scala/advent/twenty_twentytwo/Day19NotEnoughMineralsTest.scala
+++ scala-advent-of-code/src/test/scala/advent/twenty_twentytwo/Day19NotEnoughMineralsTest.scala
@@ -3,14 +3,14 @@ package advent.twenty_twentytwo
 import org.scalatest.{FlatSpec, Matchers}
 
 class Day19NotEnoughMineralsTest extends FlatSpec with Matchers {
-  "2020 Day 18 - Input" should "calculate answers" in {
+  "2020 Day 19 - Input" should "calculate answers" in {
     val dp = new Day19NotEnoughMinerals("/twenty_twentytwo/Day19-NotEnoughMinerals-input.txt")
 
     dp.part1Answer should be(994)
     dp.part2Answer should be(15960)
   }
 
-  "2020 Day 18 - Example #1" should "calculate answers" in {
+  "2020 Day 19 - Example #1" should "calculate answers" in {
     val dp = new Day19NotEnoughMinerals("/twenty_twentytwo/Day19-NotEnoughMinerals-example#1.txt")
 
     dp.part1Answer should be(33)
