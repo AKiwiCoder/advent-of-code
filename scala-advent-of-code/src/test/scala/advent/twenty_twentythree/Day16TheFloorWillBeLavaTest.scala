ea43744aa01e008b80efe6ad7dbaa916ceafb59c
Andrew Norman <akiwicoder@gmail.com>
2023-12-16 21:58:19 +1100

    Scala 2023 Day 16 - Adding missing test value

--- scala-advent-of-code/src/test/scala/advent/twenty_twentythree/Day16TheFloorWillBeLavaTest.scala
+++ scala-advent-of-code/src/test/scala/advent/twenty_twentythree/Day16TheFloorWillBeLavaTest.scala
@@ -7,7 +7,7 @@ class Day16TheFloorWillBeLavaTest extends FlatSpec with Matchers {
     val dp = new Day16TheFloorWillBeLava("/twenty_twentythree/Day16-TheFloorWillBeLava-input.txt")
 
     dp.part1Answer should be(6740)
-    dp.part2Answer should be(0)
+    dp.part2Answer should be(7041)
   }
 
   "2023 Day 16 - Example #1" should "calculate answers" in {
